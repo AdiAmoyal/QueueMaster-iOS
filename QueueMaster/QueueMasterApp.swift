cb07b960be577eba8975819dbaecddbeeeb41aec
Adi Amoyal <adi.amoyal02@gmail.com>
2023-03-23 20:21:55 +0200

    Initial Commit

---
+++ QueueMaster/QueueMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueMasterApp.swift
+//  QueueMaster
+//
+//  Created by Adi Amoyal on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct QueueMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
